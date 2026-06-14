affb683053132cfa62261b2baf8ef537aa00e14b
DevBeastTeam <devbeast143@gmail.com>
2026-06-14 09:27:57 +0500

    docs: make spam prevention guide more concise

---
+++ test-send-quick.js
@@ -0,0 +1,18 @@
+import { sendOutboundEmail } from "./smail/send-mail-from-generated-mail-from-local.js";
+
+async function run() {
+  try {
+    await sendOutboundEmail({
+      from: "yaar@llamerada.online",
+      to: "hasanameer386@gmail.com",
+      subject: "Test from DNS updated local",
+      text: "This is a test to see if the DNS changes (SPF and p=none DMARC) worked!",
+      logCallback: (msg) => console.log(msg)
+    });
+    console.log("Email sent successfully!");
+  } catch (err) {
+    console.error("Failed to send:", err.message);
+  }
+}
+
+run();
